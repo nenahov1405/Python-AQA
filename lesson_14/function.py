d4387231d4b537523a40f292ffa2411cc02b75ae
kyrylo.nenakhov <nenahov1405@gmail.com>
2025-11-02 11:58:35 +0200

    Додано тести (спроба2)

--- lesson_14/function.py
+++ lesson_14/function.py
@@ -1,32 +1,22 @@
-"""
-Ваша команда та ви розробляєте систему входу для веб-додатка,
-і вам потрібно реалізувати тести на функцію для логування подій в системі входу.
-Дано функцію, напишіть набір тестів для неї.
-"""
-
 import logging
+import pytest
+import os
 
 def log_event(username: str, status: str):
     """
     Логує подію входу в систему.
-
-    username: Ім'я користувача, яке входить в систему.
-
-    status: Статус події входу:
-
-    * success - успішний, логується на рівні інфо
-    * expired - пароль застаріває і його слід замінити, логується на рівні warning
-    * failed  - пароль невірний, логується на рівні error
+    ...
     """
-    log_message = f"Login event - Username: {username}, Status: {status}"
-
-    # Створення та налаштування логера
     logging.basicConfig(
-        filename='login_system.log',
+        filename= 'login_system.log',
         level=logging.INFO,
-        format='%(asctime)s - %(message)s'
+        format='%(asctime)s - %(message)s',
+        filemode='w',
+        force=True
         )
     logger = logging.getLogger("log_event")
+
+    log_message = f"Login event - Username: {username}, Status: {status}"
 
     # Логування події
     if status == "success":
@@ -35,3 +25,35 @@ def log_event(username: str, status: str):
         logger.warning(log_message)
     else:
         logger.error(log_message)
+
+def status_cheker(log_file):
+    """
+    Читає останнє слово з лог-файлу.
+    """
+    with open(log_file, 'r') as f:
+        data = f.read().strip()
+
+    if not data:
+        return None
+    status = data.split()[-1]
+    return status
+
+def test_logger_case_success():
+    #Перевірка логування успішного входу (success)
+    log_event("Den", "success")
+    assert status_cheker(log_file="login_system.log") == "success"
+
+def test_logger_case_expired():
+    #Перевірка логування застарілого паролю (expired)
+    log_event("Alex", "expired")
+    assert status_cheker(log_file="login_system.log") == "expired"
+
+def test_logger_case_failed():
+    #Доданий тест для невдалого входу (failed)
+    log_event("Hacker", "failed")
+    assert status_cheker(log_file="login_system.log") == "failed"
+    
+def test_logger_case_unknown_error():
+    #Доданий тест для невідомого статусу (повинен логуватися як error)
+    log_event("UnknownUser", "unknown_status")
+    assert status_cheker(log_file="login_system.log") == "unknown_status"
